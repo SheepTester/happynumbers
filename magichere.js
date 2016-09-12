43905efb02e28ba3f7351043220b359e690f2b57
Sean <ussa.say@gmail.com>
2016-09-12 15:56:46 -0700

    Magic has improved
    test it out I guess

--- magichere.js
+++ magichere.js
@@ -1,6 +1,47 @@
+var mems;
 document.querySelector("#min").onchange=function() {
   document.querySelector("#max").min=document.querySelector("#min").value;
+  if (document.querySelector("#max").value<document.querySelector("#min").value){
+    document.querySelector("#max").value=document.querySelector("#min").value;
+  }
+  if (document.querySelector("#min").value==""||document.querySelector("#min").value<0){
+    document.querySelector("#min").value=0;
+  }
+  calc();
 };
 document.querySelector("#max").onchange=function() {
   document.querySelector("#min").max=document.querySelector("#max").value;
+  if (document.querySelector("#min").value>document.querySelector("#max").value){
+    document.querySelector("#min").value=document.querySelector("#max").value;
+  }
+  if (document.querySelector("#max").value==""||document.querySelector("#max").value<0){
+    document.querySelector("#max").value=document.querySelector("#min").value;
+  }
+  calc();
 };
+function calc() {
+  mems=[];
+  document.querySelector("ul").innerHTML="";
+  for(var i=document.querySelector("#min");i<document.querySelector("#max")+1;i++){
+  	if(testForMEMS(i)){
+  		mems.push(i);
+  		document.querySelector("ul").innerHTML+="<li>"+i+"</li>";
+  	}
+  }
+  console.log(mems);
+}
+function testForMEMS(n){
+	var t;
+	while (n.toString().length>1) {
+		t=0;
+		for(var j=0;j<n.toString().length;j++){
+			t+=Number(n.toString()[j])*Number(n.toString()[j]);
+		}
+		n=t;
+	}
+	if(n==1){/* ||n==7 */
+		return true;
+	}else{
+		return false;
+	}
+}
