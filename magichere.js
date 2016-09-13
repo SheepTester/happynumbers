8c413feb3de63b023825120f0bc6f2f3aebbd59b
Sean <ussa.say@gmail.com>
2016-09-12 17:02:19 -0700

    Pop up content generator v1
    hopefully it works ;)

--- magichere.js
+++ magichere.js
@@ -22,28 +22,32 @@ function check() {
 }
 function calc() {
   mems=[];
-  var abc="abcefghijklmnopqrstuvwxyz"; /* for pop-up demonstration purposes */
+  var how;
   document.querySelector("ul").innerHTML="";
   for(var i=Number(document.querySelector("#min").value);i<Number(document.querySelector("#max").value)+1;i++){
-    if(testForMEMS(i)){
+    how=testForMEMS(i);
+    if(how!=""){
       mems.push(i);
-      document.querySelector("ul").innerHTML+="<li>"+i+"<div class='how'>"+abc+"</div></li>";
+      document.querySelector("ul").innerHTML+="<li><div class='how'>"+how+"</div>"+i+"</li>";
     }
   }
   console.log(mems);
 }
 function testForMEMS(n){
-  var t;
+  var t,wow="";
   while (n.toString().length>1) {
     t=0;
     for(var j=0;j<n.toString().length;j++){
       t+=Number(n.toString()[j])*Number(n.toString()[j]);
+      wow+=n.toString()[j]+"<sup>2</sup>+";
     }
     n=t;
+    wow=wow.slice(0,wow.length-1);
+    wow+="=<b>"+n+"</b><br>";
   }
   if(n==1||n==7){
-    return true;
+    return wow;
   }else{
-    return false;
+    return "";
   }
 }
